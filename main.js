8e09be40ee4db48ca2302272d52f8c589ec20258
Kaushal <Kaushal23@gmail.com>
2024-01-29 03:09:50 +0530

    hero section done

---
+++ main.js
@@ -0,0 +1,9 @@
+// For Typing Effect in Hero Section
+
+var typed = new Typed('#textline', {
+    strings: ["Frontend Developer", "GCP Enthusiast", "Graphic Designer", "CS Grad"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    bacDelay: 1000,
+    loop: true
+  });
