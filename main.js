1e7303d4c1dc443703cc18327b24326f23edcfea
Kaushal Kishore <kaushalsonic26@gmail.com>
2024-01-29 23:42:43 +0530

    Completed responsiveness hero section

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ var typed = new Typed('#textline', {
     strings: ["Frontend Developer", "GCP Enthusiast", "Graphic Designer", "CS Grad"],
     typeSpeed: 70,
     backSpeed: 70,
-    bacDelay: 1000,
+    bacDelay: 1500,
     loop: true
   });
+
+// For Toggling Light Mode
+
+function colorMode(){
+  let mode = document.body;
+  mode.classList.toggle("light-mode");
+}
