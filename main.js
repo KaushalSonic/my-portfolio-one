f0a343a5a1761ce17224bb673dfa7552b6863b89
Kaushal Kishore <kaushalsonic26@gmail.com>
2024-01-31 02:29:14 +0530

    update check

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // For Typing Effect in Hero Section
 
 var typed = new Typed('#textline', {
-    strings: ["Frontend Developer", "GCP Enthusiast", "Graphic Designer", "CS Grad"],
+    strings: ["Front-End Developer", "GCP Enthusiast", "Graphic Designer", "CS Grad"],
     typeSpeed: 70,
     backSpeed: 70,
     bacDelay: 1500,
@@ -13,4 +13,11 @@ var typed = new Typed('#textline', {
 function colorMode(){
   let mode = document.body;
   mode.classList.toggle("light-mode");
+}
+
+// For expanding more details in about section (Always use ID instead of class for display block)
+
+function details(){
+  const details = document.getElementById("more-details");
+  details.style.display = "block";
 }
